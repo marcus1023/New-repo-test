3cfb8a16abacfae1948000451b3d5e1bc367f3b5
Marcus Ogden <marcus@uerlite.com>
2016-11-28 15:19:23 -0500

    newsest addition

---
+++ file2.js
@@ -0,0 +1,7 @@
+var journal = [
+
+"today is the first day of devmountain and I am cruching it"
+"this is the second journal entry and its only a test"
+"this is the third and final test of the commit system"
+
+]
